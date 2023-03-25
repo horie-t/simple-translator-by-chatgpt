71d0afb8366de9b8139593eb40a45c819a7baa70
HORIE Tetsuya <horie-t@users.noreply.github.com>
2023-03-25 20:04:05 +0900

    Spring Webのプロジェクトを作成

---
+++ backend/src/test/java/com/tehorie/simple_translator/SimpleTranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tehorie.simple_translator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTranslatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
