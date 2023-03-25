42b3ae8962111af1b2163e07e9698e2cc480e669
HORIE Tetsuya <horie-t@users.noreply.github.com>
2023-03-25 21:27:59 +0900

    翻訳処理にGoogle Cloud Translationを使う

--- backend/src/main/java/com/tehorie/simple_translator/service/TranslationService.java
+++ backend/src/main/java/com/tehorie/simple_translator/service/TranslationService.java
@@ -1,13 +1,32 @@
 package com.tehorie.simple_translator.service;
 
+import com.google.cloud.translate.Translate;
+import com.google.cloud.translate.TranslateOptions;
+import com.google.cloud.translate.Translation;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TranslationService {
 
-    // このサンプルメソッドは、実際の翻訳APIを呼び出す代わりにダミーの翻訳を返します。
+    @Value("${translation.apiKey:${TRANSLATION_API_KEY}}")
+    private String apiKey;
+
     public String translate(String text, String sourceLanguage, String targetLanguage) {
-        return "Translated Text: " + text + ", Source Language: " + sourceLanguage + ", Target Language: " + targetLanguage;
+        // APIキーを使用して、Google Cloud Translation APIクライアントを作成します。
+        Translate translate = TranslateOptions.newBuilder()
+                .setApiKey(apiKey)
+                .build()
+                .getService();
+
+        // リクエストの構成を指定して、翻訳を実行します。
+        Translation translation = translate.translate(
+                text,
+                Translate.TranslateOption.sourceLanguage(sourceLanguage),
+                Translate.TranslateOption.targetLanguage(targetLanguage)
+        );
+
+        // 翻訳されたテキストを返します。
+        return translation.getTranslatedText();
     }
 }
-
